e0d65b2b54b8fbe1f308f89e24c64400485952af
Lydia <lydiadoula@gmail.com>
2022-10-12 23:02:57 +0100

    JS bætt við

---
+++ main.js
@@ -0,0 +1,58 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let username = id("username"),
+  password = id("password"),
+  form = id("form"),
+  
+  errorMsg = classes("error"),
+  successIcon = classes("success-icon"),
+  failureIcon = classes("failure-icon");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    engine(username, 0, "Please type username");
+    engine(password, 1, "Please type password");
+  });
+
+
+  let engine = (id, serial, message) => {
+
+    if (id.value.trim() === "") {
+        errorMsg[serial].innerHTML = message;
+        id.style.border = "2px solid red";
+
+        failureIcon[serial].style.opacity = "1";
+        successIcon[serial].style.opacity = "0";
+    } 
+    
+    else {
+        errorMsg[serial].innerHTML = "";
+        id.style.border = "2px solid green";
+
+        failureIcon[serial].style.opacity = "0";
+        successIcon[serial].style.opacity = "1";
+    }
+  }
